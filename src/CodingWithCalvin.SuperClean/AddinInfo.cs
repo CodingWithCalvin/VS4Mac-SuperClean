e6727c17d6c620fd014987927fbdb4b4af834861
Calvin A. Allen <calvin.allen@outlook.com>
2024-05-14 16:21:13 -0400

    Update AddinInfo.cs

--- src/CodingWithCalvin.SuperClean/AddinInfo.cs
+++ src/CodingWithCalvin.SuperClean/AddinInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Mono.Addins;
 
-[assembly: Addin(Id = "SuperClean", Namespace = "CodingWithCalvin", Version = "2023.07.07.1")]
+[assembly: Addin(Id = "SuperClean", Namespace = "CodingWithCalvin", Version = "1.0")]
 [assembly: AddinName("Super Clean")]
 [assembly: AddinCategory("IDE extensions")]
 [assembly: AddinDescription("A Visual Studio for Mac (2022+) extension that adds a menu command to Build menu, that triggers a recursive delete (i.e., clean, but super) on all projects in the solution, a right-click context menu option on the Project node, and a right-click context menu option on the Solution node.")]
